57c8bbe1ad59b0bd98f252d44e591239834210cc
Sundaravel-3003 <110889723+Sundaravel-3003@users.noreply.github.com>
2022-09-01 14:42:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function foo(){
+    var res = document.getElementById("fn").value;
+    console.log(res);
+    var res1 = document.getElementById("mn").value;
+    console.log(res1);
+    var res2 = document.getElementById("ln").value;
+    console.log(res2);
+    var res3 = document.getElementById("Em").value;
+    console.log(res3);
+
+}
